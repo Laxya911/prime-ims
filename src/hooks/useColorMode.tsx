151c2bb41ac1d129c0f8afd5f1a6b8390d8c0bb3
Laxya911 <laxuaryal@gmail.com>
2023-10-06 00:13:08 +0530

    fix window is not defined

--- src/hooks/useColorMode.tsx
+++ src/hooks/useColorMode.tsx
@@ -2,16 +2,34 @@
 import { useEffect } from "react";
 import useLocalStorage from "./useLocalStorage";
 
+// const useColorMode = () => {
+//   const [colorMode, setColorMode] = useLocalStorage("color-theme", "light");
+
+//   useEffect(() => {
+//     const className = "dark";
+//     const bodyClass = window.document.body.classList;
+
+//     colorMode === "dark"
+//       ? bodyClass.add(className)
+//       : bodyClass.remove(className);
+//   }, [colorMode]);
+
+//   return [colorMode, setColorMode];
+// };
+
+// export default useColorMode;
 const useColorMode = () => {
   const [colorMode, setColorMode] = useLocalStorage("color-theme", "light");
 
   useEffect(() => {
-    const className = "dark";
-    const bodyClass = window.document.body.classList;
+    if (typeof window !== "undefined") {
+      const className = "dark";
+      const bodyClass = window.document.body.classList;
 
-    colorMode === "dark"
-      ? bodyClass.add(className)
-      : bodyClass.remove(className);
+      colorMode === "dark"
+        ? bodyClass.add(className)
+        : bodyClass.remove(className);
+    }
   }, [colorMode]);
 
   return [colorMode, setColorMode];
